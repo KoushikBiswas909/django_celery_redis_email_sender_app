f18969450072d8aa2700465669498a7ff885a195
KoushikBiswas909 <koushikb909@gmail.com>
2022-06-01 22:35:45 +0530

    first commit

---
+++ example/tasks.py
@@ -0,0 +1,20 @@
+from celery import shared_task
+from time import sleep
+
+from django.core.mail import send_mail
+
+
+@shared_task
+def sleepy(duration):
+    sleep(duration)
+    return None
+
+
+@shared_task
+def send_email_task():
+    sleep(10)
+    send_mail('Celery Task Worked!',
+              'This is proof the task worked!',
+              'monbiswas909@gmail.com',
+              ['abuh69377@gmail.com'])
+    return None
