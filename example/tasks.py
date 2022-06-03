09d938fb80876c3de5f48289e3240d2b5ad65a31
KoushikBiswas909 <koushikb909@gmail.com>
2022-06-03 21:13:04 +0530

    Part 2 where i send mail to any no of times

--- example/tasks.py
+++ example/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from time import sleep
-
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
 from django.core.mail import send_mail
 
 
@@ -13,8 +14,10 @@ def sleepy(duration):
 @shared_task
 def send_email_task():
     sleep(10)
-    send_mail('Celery Task Worked!',
-              'This is proof the task worked!',
-              'monbiswas909@gmail.com',
-              ['abuh69377@gmail.com'])
+    msg_html = render_to_string('templates/mail.html', None)
+    for x in range(2):
+        send_mail('Celery Task Worked!'+str(x),
+                  'Celery is working totally fine',
+                  'monbiswas909@gmail.com',
+                  ['danielkaushik1@gmail.com'])
     return None
